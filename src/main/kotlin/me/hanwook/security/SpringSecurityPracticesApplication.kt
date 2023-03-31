6dafb74b8f3a551f330880b528db0b67d5362ade
Hanwook Ryu <ryu@hanwook.me>
2023-03-31 12:46:33 +0900

    Setting: 기본적인 스프링 부트 프로젝트 설정

---
+++ src/main/kotlin/me/hanwook/security/SpringSecurityPracticesApplication.kt
@@ -0,0 +1,11 @@
+package me.hanwook.security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityPracticesApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityPracticesApplication>(*args)
+}
---
+++ src/test/kotlin/me/hanwook/security/SpringSecurityPracticesApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.hanwook.security
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityPracticesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
