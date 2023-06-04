58a710087a02de023b53f3c6ec221bca9cfb631d
Hanwook Ryu <ryu@hanwook.me>
2023-06-04 22:58:01 +0900

    Add: 스프링 시큐리티 - 첫번째 프로젝트

---
+++ first-project/src/main/kotlin/me/hanwook/security/firstproject/controller/HelloController.kt
@@ -0,0 +1,11 @@
+package me.hanwook.security.firstproject.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello() = "Hello!"
+}
